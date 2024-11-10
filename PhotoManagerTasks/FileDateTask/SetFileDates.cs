8f2037b9d5d5e6b7d16413d37cab7eea75ddab28
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-11-10 17:35:33 +0100

    Add custom tasks

---
+++ PhotoManagerTasks/FileDateTask/SetFileDates.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Build.Framework;
+
+namespace FileDateTask;
+
+public class SetFileDates : ITask
+{
+    // List of files to apply the date
+    [Required] public ITaskItem[] Files { get; set; } = [];
+
+    // The date to set on the files
+    [Required] public string? Date { get; set; } = string.Empty;
+
+    public bool Execute()
+    {
+        try
+        {
+            DateTime fixedDate = DateTime.Parse(Date ?? string.Empty);  // Parse the provided date
+
+            foreach (ITaskItem file in Files)
+            {
+                string filePath = file.ItemSpec;
+                if (File.Exists(filePath))
+                {
+                    // Set both the creation and last modified times
+                    File.SetCreationTime(filePath, fixedDate);
+                    File.SetLastWriteTime(filePath, fixedDate);
+                }
+            }
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public IBuildEngine? BuildEngine { get; set; }
+    public ITaskHost? HostObject { get; set; }
+}
---
+++ PhotoManagerTasks/FileExtractionTask/ExtractRarFile.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Build.Framework;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Rar;
+
+namespace FileExtractionTask;
+
+public class ExtractRarFile : ITask
+{
+    // The full path to the .rar file to extract
+    [Required] public string? RarFilePath { get; set; } = string.Empty;
+
+    // The path where the extracted contents will be placed
+    [Required] public string? DestinationPath { get; set; } = string.Empty;
+
+    public bool Execute()
+    {
+        try
+        {
+            if (!Directory.Exists(DestinationPath))
+            {
+                Directory.CreateDirectory(DestinationPath ?? string.Empty);
+            }
+
+            using (RarArchive archive = RarArchive.Open(RarFilePath ?? string.Empty))
+            {
+                foreach (RarArchiveEntry entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                {
+                    entry.WriteToDirectory(DestinationPath ?? string.Empty, new SharpCompress.Common.ExtractionOptions
+                    {
+                        ExtractFullPath = true,
+                        Overwrite = true
+                    });
+                }
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public IBuildEngine? BuildEngine { get; set; }
+    public ITaskHost? HostObject { get; set; }
+}
